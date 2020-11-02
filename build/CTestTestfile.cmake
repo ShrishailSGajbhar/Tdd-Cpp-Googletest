a3e51ade5803f4daa73fa76c27b39c82f7d0db6b
Shrishail <shrishailgajbhar5@gmail.com>
2020-11-03 00:00:38 +0530

    Add the Tdd using Cpp & GTest

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shrishail/workspace/cpp/tdd_fizzbuzz
-# Build directory: /home/shrishail/workspace/cpp/tdd_fizzbuzz/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
